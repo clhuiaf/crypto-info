88fca25b60304f2d4ac93898fff050172923236b
clhuiaf <clhuiaf777@gmail.com>
2026-01-26 17:17:57 +0800

    fix : add back extra closing div in prices pages

--- app/prices/large-caps/page.tsx
+++ app/prices/large-caps/page.tsx
@@ -37,5 +37,6 @@ export default function LargeCapsPage() {
         </Link>
       </div>
     </div>
+  </div>
   );
 }
--- app/prices/top-movers/page.tsx
+++ app/prices/top-movers/page.tsx
@@ -37,5 +37,6 @@ export default function TopMoversPage() {
         </Link>
       </div>
     </div>
+  </div>
   );
 }
